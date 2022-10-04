3ed54af40cbce3a035d247ea44b0c5e7107c81f1
Ludmila Moreira <ludmila.moreira@loggi.com>
2022-10-04 16:49:01 -0300

    create repository one-planet

---
+++ src/main/kotlin/com/oneplanet/Application.kt
@@ -0,0 +1,8 @@
+package com.oneplanet
+
+import io.micronaut.runtime.Micronaut.*
+
+fun main(args: Array<String>) {
+	run(*args)
+}
+
---
+++ src/test/kotlin/com/oneplanet/OnePlanetTest.kt
@@ -0,0 +1,19 @@
+package com.oneplanet
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+
+@MicronautTest
+class OnePlanetTest {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
